d2daf185c0ff4e1ade9d7df90f7063cfc19e52f7
Antoine <a.fourcade65@gmail.com>
2023-12-19 15:49:45 +0100

    style: changed project font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,12 @@
 module.exports = {
   important: true,
   content: ['./index.templ', './src/templates/**/*.templ'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'cabin': ['"Cabin"', 'sans-serif']
+      }
+    }
+  },
   plugins: [],
 }
